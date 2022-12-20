c47821400cc74d5b252f14946a0e1cb3c383a330
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2022-12-20 21:08:44 +0100

    2 task ready (esc ++)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryDiv = document.querySelector('.gallery');
+const gallaryMarkup = createGalaryMarkup(galleryItems);
+
+galleryDiv.addEventListener('click', onGallaryClick);
+galleryDiv.insertAdjacentHTML('beforeend', gallaryMarkup);
+
+
+function createGalaryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${preview}">
+        <img
+      class="gallery__image"
+      src="${original}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </div>`
+    }).join('')
+};
+
+
+function onGallaryClick(event) {
+  event.preventDefault();
+  
+  const isGallarySwatchEl = event.target.classList.contains('gallery__image');
+  if (!isGallarySwatchEl) {
+    return;
+  };
+
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+  instance.show();
+  window.addEventListener('keydown', onEsc);
+    
+
+  function onEsc(event) {
+
+    if (event.key === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', onEsc);
+    };
+  };
+  
+}
+
+  
